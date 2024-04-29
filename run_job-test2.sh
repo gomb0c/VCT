cc6029acb8572a5e6e37a5ad1a4a83654e36cf38
gomb0c <z5165555@student.unsw.edu.au>
2024-04-29 23:14:37 +1000

    WIP

--- run_job-test2.sh
+++ run_job-test2.sh
@@ -10,16 +10,16 @@
 #PBS -l storage=gdata/po67
 #PBS -l wd
 
-echo BEFORE_LOADING > /g/data/po67/anonymous_cat/Trained/logs/log1.log
+echo BEFORE_LOADING > /g/data/po67/anonymous_cat/Trained/logs/test2-log1.log
 module load python3/3.9.2
 module load pytorch/1.9.0
 
-echo LOADED_MODULES > /g/data/po67/anonymous_cat/Trained/logs/log2.log
+echo LOADED_MODULES > /g/data/po67/anonymous_cat/Trained/logs/test2-log2.log
 ###############################################################
 cp -r /g/data/po67/anonymous_cat/Code/VCT $PBS_JOBFS
-echo COPIED SUCCESSFULLY > /g/data/po67/anonymous_cat/Trained/logs/log3.log
+echo COPIED SUCCESSFULLY > /g/data/po67/anonymous_cat/Trained/logs/test2-log3.log
 export PYTHONPATH="${PYTHONPATH}:/g/data/po67/anonymous_cat/lib/python3.9/site-packages/"
-echo EXPORTED_PATH > /g/data/po67/anonymous_cat/Trained/logs/log4.log
+echo EXPORTED_PATH > /g/data/po67/anonymous_cat/Trained/logs/test2-log4.log
 cd $PBS_JOBFS
 cd VCT
 
@@ -30,7 +30,7 @@ cd VCT
 # otherwise it gets lost in $PBS_JOBFS once job is finished
 
 # e.g., python3 main.py > /g/data/kf26/$USER/logs
-ls > /g/data/po67/anonymous_cat/Trained/logs/log5.log
+ls > /g/data/po67/anonymous_cat/Trained/logs/test2-log5.log
 python3 main_vqvae.py --epochs 200 --dataset mpi_real --model vqvae --data-dir /g/data/po67/anonymous_cat/Data/ > /g/data/po67/anonymous_cat/Trained/logs/job2_output.log
 
 #################################################################
